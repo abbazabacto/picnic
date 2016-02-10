25e0d3ba6d9c6a7f9e3d99bc3ace925984660516
Bart Meinders <abbazabacto@gmail.com>
2016-02-10 22:45:09 +0100

    add angular-resource module

--- karma.conf.js
+++ karma.conf.js
@@ -12,6 +12,7 @@ module.exports = function(config) {
     files: [
       'node_modules/angular/angular.js',
       'node_modules/angular-mocks/angular-mocks.js',
+      'node_modules/angular-resource/angular-resource.js',
       'node_modules/angular-route/angular-route.js',
       'node_modules/angular-i18n/angular-locale_nl-nl.js',
       'src/picnic.js',
--- src/picnic.js
+++ src/picnic.js
@@ -2,6 +2,7 @@ import angular from 'angular';
 import picnicApp from './picnic.app';
 
 const picnic = angular.module('picnic', [
+  'ngResource',
   picnicApp.name
 ]);
 
